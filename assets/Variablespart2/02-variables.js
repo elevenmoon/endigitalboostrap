13b5fab2066db36c77f7bc5d237d07adafaf2e5e
ninemoon <roman.edgardogarcia@gmail.com>
2024-10-26 11:25:46 -0400

    add operation

--- assets/Variablespart2/02-variables.js
+++ assets/Variablespart2/02-variables.js
@@ -1,3 +1,8 @@
 var pais = 'Venezuela';
 var ciudad = 'San juan de los morros';
-console.log(pais);
+console.log(pais); //imprimir en consola
+
+var pais_ciudad = pais+' '+ciudad; //concatenacion de variables de tipo string
+
+console.log(pais_ciudad);
+
---
+++ assets/variblepart3/operacionesbasica.js
@@ -0,0 +1,24 @@
+var n1=10;
+var n2=25;
+var suma=n1+n2;
+console.log(suma);
+var resta=n1-n2;
+console.log(resta);
+var multiplica=n1*n2;
+console.log(multiplica);
+var division=n1/n2;
+console.log(division);
+suma = n1 + n2;
+alert("la suma de"+n1+"+"+n2+"="+suma);
+console.log("la suma de",n1,"+",n2,"=", suma);
+
+alert("la resta de"+n1+"-"+n2+"="+resta);
+console.log("la resta de",n1,"-",n2,"=", resta);
+
+alert("la multiplicacion de"+n1+"*"+n2+"="+multiplica);
+console.log("la multiplicacion de"+n1+"*"+n2+"=", multiplica);
+
+alert("la division de"+n1+"/"+n2+"="+multiplica);
+console.log("la division de"+n1+"/"+n2+"="+division);
+
+
